7147d90f7222beebd4f73c7a73f5d558f6c91509
Stefan Kupresak <stefan_vg@hotmail.com>
2020-07-24 18:11:07 +0200

    finish enemy moving towards tower

--- enemies/enemy_base.gd
+++ enemies/enemy_base.gd
@@ -1,4 +1,14 @@
 extends KinematicBody2D
 
+var target = null
+
 func _ready():
 	$Animation.play("idle")
+
+func _physics_process(_delta):
+	if target:
+		var desired = target - global_position
+		move_and_slide(desired.normalized() * 35)
+
+func set_target(new_target):
+	target = new_target
